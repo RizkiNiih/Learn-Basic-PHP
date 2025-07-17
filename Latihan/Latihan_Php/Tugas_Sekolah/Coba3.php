2c071e079411051c4d09934e7c5f92801c057156
RizkiNiih <rizkigaming0204@gmail.com>
2025-07-17 20:52:15 +0700

    Operator Assigment

---
+++ Latihan/Latihan_Php/Tugas_Sekolah/Coba3.php
@@ -0,0 +1,17 @@
+<?php 
+echo "<h1>Operator Assigment</h1><br>";
+$x = 4;
+echo "x = $x<br>";
+$x += 3;
+echo "hasil dari operasi x += 3 adalah = $x<br>";
+$x -= 3;
+echo "hasil dari operasi x -= 3 adalah = $x<br>";
+$x *= 3;
+echo "hasil dari operasi x *= 3 adalah = $x<br>";
+$x /= 3;
+echo "hasil dari operasi x /= 3 adalah = $x<br>";
+$x %= 3;
+echo "hasil dari operasi x %= 3 adalah = $x<br>";
+$x .= 3;
+echo "hasil dari operasi x .= 3 adalah = $x<br>";
+?>
