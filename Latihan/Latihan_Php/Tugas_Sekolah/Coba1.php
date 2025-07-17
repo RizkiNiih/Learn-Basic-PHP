f3465f77fdc662b072e0deaf973638ede14ed2a0
RizkiNiih <rizkigaming0204@gmail.com>
2025-07-17 14:45:02 +0700

    Latihan pemograman struktur1

---
+++ Latihan/Latihan_Php/Tugas_Sekolah/Coba1.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>Variabel dan Tipe Data</h1><br>";
+a = 5; // variabel bertipe integer
+$b = 2.5; // variabel bertipe float
+$komentar = "Selamat datang di pemrograman web"; // variabel bertipe string
+echo "Nilai variabel a = $a<br>"; // menampilkan variabel a
+echo "Nilai variabel b = $b<br>"; // menampilkan variabel b
+echo "Nilai variabel komentar = $komentar<br>"; // menampilkan variabel komentar
+?>
