96af00ba4210b5fc92191e9f9b7a116c91183af6
RizkiNiih <rizkigaming0204@gmail.com>
2025-07-21 07:44:36 +0700

    Operasi String

---
+++ Latihan/Latihan_Php/Tugas_Sekolah/Coba7.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>Operasi String</h1><br>"; 
+$comment1 = "Hello...!"; 
+$comment2 = "Selamat datang di pemrograman web"; 
+$comment3 = $comment1 . " " . $comment2; 
+echo "Kalimat 1: " . $comment1 . "<br>"; 
+echo "Kalimat 2: " . $comment2 . "<br>"; 
+echo "Gabungan kalimat: " . $comment3 . "<br>"; 
+?>
