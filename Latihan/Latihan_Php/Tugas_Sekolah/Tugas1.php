9d9015e9fad4fed43107be241ce8e2b41d5ce15b
RizkiNiih <rizkigaming0204@gmail.com>
2025-07-30 04:10:39 +0700

    rumus menghitung luas dan keliling persegi

--- Latihan/Latihan_Php/Tugas_Sekolah/Tugas1.php
+++ Latihan/Latihan_Php/Tugas_Sekolah/Tugas1.php
@@ -1,12 +1,21 @@
 <?php
-$alas = 6;
-$tinggi = 8;
+// $alas = 6;
+// $tinggi = 8;
 
-$miring = sqrt(pow($alas, 2) + pow($tinggi, 2));
-$luas = 0.5 * $alas * $tinggi;
-$keliling = $alas + $tinggi + $miring;
+// $miring = sqrt(pow($alas, 2) + pow($tinggi, 2));
+// $luas = 0.5 * $alas * $tinggi;
+// $keliling = $alas + $tinggi + $miring;
 
-echo "Alas: $alas <br>";
-echo "Luas Segitiga: $luas <br>";
-echo "Keliling Segitiga: " . round($keliling, 2) . "<br>";
+// echo "Alas: $alas <br>";
+// echo "Luas Segitiga: $luas <br>";
+// echo "Keliling Segitiga: " . round($keliling, 2) . "<br>";
+
+
+$sisi = 4;
+
+$luas = $sisi * $sisi;
+$keliling = 4 * $sisi;
+
+echo "Luas Persegi: $luas <br>";
+echo "Keliling Persegi: " . round($keliling, 2) . "<br>";
 ?>
