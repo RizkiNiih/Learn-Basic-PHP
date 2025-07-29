c294c463e3d1b85459d95a3af4f5c7ad0d83add9
RizkiNiih <rizkigaming0204@gmail.com>
2025-07-30 04:02:30 +0700

    Rumus menghitung luas dan keliling segitiga siku - siku

---
+++ Latihan/Latihan_Php/Tugas_Sekolah/Tugas1.php
@@ -0,0 +1,12 @@
+<?php
+$alas = 6;
+$tinggi = 8;
+
+$miring = sqrt(pow($alas, 2) + pow($tinggi, 2));
+$luas = 0.5 * $alas * $tinggi;
+$keliling = $alas + $tinggi + $miring;
+
+echo "Alas: $alas <br>";
+echo "Luas Segitiga: $luas <br>";
+echo "Keliling Segitiga: " . round($keliling, 2) . "<br>";
+?>
